480d055b74f98925d7bc1e693ae6dac8848deaa5
Taras Shpek <taras.shpek@gmail.com>
2021-09-20 23:18:34 +0300

    add autoconfiguration

---
+++ src/main/java/software/plusminus/type/config/TypeConfig.java
@@ -0,0 +1,9 @@
+package software.plusminus.type.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("software.plusminus.type")
+public class TypeConfig {
+}
